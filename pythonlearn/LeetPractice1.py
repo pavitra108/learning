533bf10d60a6aed4337925c9dcd7e4b53f3db27f
Pon Pavita Jothi <pavitra@Pavitra-MacBook-Pro.local>
2023-08-30 19:21:39 -0400

    update new sum

--- pythonlearn/LeetPractice1.py
+++ pythonlearn/LeetPractice1.py
@@ -10,26 +10,36 @@ def merge(nums1, m, nums2, n):
     nums1.sort()
     print(nums1)
 
-a= [1,2,4,0]
-b= [3,7,0]
+
+a = [1, 2, 4, 0]
+b = [3, 7, 0]
 ans = merge(a, 4, b, 3)
+
 
 # create a dictionary
 # split words in a string
 # iterate each word and check if it there in dic as value
 # if word is there, plus one else just 1
-# word right before full stop - word and full stop taken as one string
+# word right before full stop - word and full stop taken as one string(not done
+# took the first three index entries from sorted dic
 
-def countWord(para):
+def count_word(para):
     word = para.split()
     word_dic = {}
     for each_word in word:
         if each_word in word_dic:
-            word_dic[each_word] +=1
+            val = word_dic[each_word]
+            val = val+1
+            word_dic[each_word] = val
         else:
-            word_dic[each_word] =1
-    print(word_dic)
+            word_dic[each_word] = 1
+    return word_dic
 
-ans = countWord("All Iron Man Movies. Three Iron Man movies. However, Tony Stark played a big part in the MCU and was also one of the key characters in six other movies in the franchise. Iron Man appeared in 9 movies and had one uncredited cameo.")
+def custom_fn(input):
+    return input[1]
+ans = count_word("All Iron Man Movies. Three Iron Man movies. However, Tony Stark played a big part in the MCU and was also one of the key characters in six other movies in the franchise. Iron Man appeared in 9 movies and had one uncredited cameo.")
+sorted_dic = dict(sorted(ans.items(), key=custom_fn, reverse=True)[:3])
+ok = print(sorted_dic)
+
 
 
