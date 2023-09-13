a36ed3ba914cda429a09d46a489a99e9ff1e453d
Pon Pavita Jothi <pavitra@Pavitra-MacBook-Pro.local>
2023-09-13 19:17:27 -0400

    python course quiz

---
+++ pythonlearn/CourseQuiz.py
@@ -0,0 +1,40 @@
+# return True if all of the letters are efficient
+# retuen False if any of the letters are not efficient
+# a letter is "efficient" if it is in the collection "BCDGIJLMNOPSUVWZ".
+
+def is_efficient(letters):
+    eff_letters = set('BCDGIJLMNOPSUVWZ')
+    for let in letters:
+        if let in eff_letters:
+            pass
+        else:
+            return False
+    return True
+see = is_efficient("bcdg")
+print(see)
+
+
+# swaps the keys and values in a dictionary
+# a given value might be mapped to by more than one key
+
+def swap_keys_and_values(d):
+    out_dict = dict()
+    for key, value in d.items():
+        if value in out_dict:
+            hh = out_dict[value]
+            tt = d[key]
+            out_dict[value] = f"{hh}, {key}"
+        else:
+            out_dict[value] = key
+    print(out_dict)
+
+swap_keys_and_values({
+    'apple': 5,
+    'banana': 2,
+    'orange': 8,
+    'grape': 3,
+    'berry': 2,
+    'kiwi': 5
+    })
+
+
