d98e0bfff973a32d9dbc5fca4ab286ecdbb5e6d1
Pon Pavita Jothi <pavitra@Pavitra-MacBook-Pro.local>
2023-09-14 14:28:27 -0400

    update sway key sum

--- pythonlearn/CourseQuiz.py
+++ pythonlearn/CourseQuiz.py
@@ -18,23 +18,23 @@ print(see)
 # a given value might be mapped to by more than one key
 
 def swap_keys_and_values(d):
+    set_dict = {key: {value} for key, value in d.items()}
     out_dict = dict()
     for key, value in d.items():
         if value in out_dict:
             hh = out_dict[value]
-            tt = d[key]
-            out_dict[value] = f"{hh}, {key}"
+            hh.add(key)
+            out_dict[value] = hh
         else:
-            out_dict[value] = key
+            out_dict[value] = {key}
     print(out_dict)
 
 swap_keys_and_values({
     'apple': 5,
     'banana': 2,
     'orange': 8,
-    'grape': 3,
+    'grape': 2,
     'berry': 2,
     'kiwi': 5
     })
-
 
