1826f6cf65f18deefc3c798bd5ee00ec8be19d5e
Pon Pavita Jothi <pavitra@Pavitra-MacBook-Pro.local>
2023-09-18 12:35:37 -0400

    sum on variadic arguments

--- pythonlearn/PracticeSums.py
+++ pythonlearn/PracticeSums.py
@@ -16,4 +16,30 @@ def find_largest(nums):
     print(f"{largest} is the largest")
 
 
-ans=find_largest([])
+ans=find_largest([4, 7, 2, 8, 4, 5])
+
+
+#if the letters in caps are available in the list of given 'efficient' letters
+def is_efficient(letters):
+    eff_letters = set('BCDGIJLMNOPSUVWZ')
+    for let in letters:
+        if let in eff_letters:
+            pass
+        else:
+            return False
+    return True
+
+see = is_efficient("BCDGIJ")
+print(see)
+
+#at least one positional argument (someone's given name, and a variadic collection of surnames or modifiers)
+# and must handle a variadic collection of keyword-specified arguments with details from a profile.
+
+def create_profile(given_name, *surname, **profile):
+    for each in surname:
+        print(given_name, each)
+    for key, value in profile.items():
+        print(key, value, sep=': ')
+
+
+create_profile("Sebastian", "Thrun", cofounded="Udacity", experience="Stanford Professor")
