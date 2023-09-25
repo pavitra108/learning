e8811fab70208f0028632223645a88dd8520cb36
Pon Pavita Jothi <pavitra@Pavitra-MacBook-Pro.local>
2023-09-25 18:39:05 -0400

    decorators sum added

--- pythonlearn/CourseQuiz.py
+++ pythonlearn/CourseQuiz.py
@@ -38,3 +38,58 @@ swap_keys_and_values({
     'kiwi': 5
     })
 
+# find anagrams exercise
+
+english_words_small = {'open', 'peon', 'nope', 'stone', 'notes', 'onset', 'tones', 'cone', 'pots', 'post', 'stop',
+                       'opts', 'tops'}
+
+def load_words_from_filename(filename):
+    """Load a set of words from a newline-separated file."""
+    with open(filename) as infile:
+        return set(line.strip().lower() for line in infile)
+
+
+def find_anagrams(letters, words):
+    english_words_small = {'open', 'peon', 'nope', 'stone', 'notes', 'onset', 'tones', 'cone', 'pots', 'post', 'stop', 'opts', 'tops'}
+
+#sum on decorators
+#.catche the output
+
+import functools
+import time
+
+
+catch_dict = {}
+def dummy(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        if function.__name__ in catch_dict:
+           value_dict = catch_dict[function.__name__]
+           n_key = (*args, tuple(kwargs.items()))
+           if n_key in value_dict:
+               return value_dict[n_key]
+           else:
+               one_output = function(*args, **kwargs)
+               value_dict[n_key] = one_output
+               return one_output
+        else:
+            one_dict ={}
+            catch_dict[function.__name__] = one_dict
+            output = function(*args, **kwargs)
+            one_dict[(*args, tuple(kwargs.items()))] = output
+            return output
+    return wrapper
+
+@dummy
+def addmy(x, y):
+    time.sleep(5)
+    return x+y
+
+@dummy
+def sub(x,y):
+    time.sleep(5)
+    return x-y
+
+print(addmy(1,3))
+print(addmy(1,5))
+print(sub(1,3))
