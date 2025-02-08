9ee1cc9e7be81631a1623e6f1d35d180af38d12e
pavitra108 <pavitra.viswesh5@gmail.com>
2025-02-08 18:22:37 -0500

    display answer pending

--- currency-converter/main.py
+++ currency-converter/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_file
-
-
+from rates import get_exchange_rates
 
 # The application code
 app = Flask(__name__)
@@ -12,20 +11,23 @@ def index():
 
 
 @app.route('/convert', methods=['POST'])
-def abc():
-    if request.method == "POST":
-        from_currency = request.form["from_currency"]
-        to_currency = request.form["to_currency"]
-        amount = float(request.form["amount"])
+def convert_currency():
+    # Convert given currency based on the latest exchange rates.
+    f_currency = request.form["from_currency"]
+    t_currency = request.form["to_currency"]
+    amount = float(request.form["amount"])
 
-        # Get currency exchange rates
+    # Get currency exchange rates
+    value = get_exchange_rates()
 
+    from_currency_rates = value['rates'].get(f_currency, None)
+    to_currency_rates = value['rates'].get(t_currency, None)
 
-        # Convert the amount
-        converted_amount = amount * (currency_rates[to_currency] / currency_rates[from_currency])
-
-        return render_template("converter-page.html", converted_amount=converted_amount,
-                               from_currency=from_currency, to_currency=to_currency, amount=amount)
+    # Convert the amount
+    converted_amount = amount * (to_currency_rates/from_currency_rates)
+    print(f"{amount} {f_currency} is equal to {converted_amount} {t_currency}")
+    return render_template("converter-page.html", converted_amount=converted_amount,
+                           from_currency=f_currency, to_currency=t_currency, amount=amount)
 
 
 if __name__ == "__main__":
--- currency-converter/rates.py
+++ currency-converter/rates.py
@@ -4,7 +4,6 @@ import os
 
 def get_exchange_rates():
     app_id = os.getenv('APP_ID')
-    print(app_id)
     url = f"https://openexchangerates.org/api/latest.json?app_id=d69755469a4c4784a02221e1342762e7"
     headers = {"accept": "application/json"}
 
@@ -14,3 +13,5 @@ def get_exchange_rates():
 
 a = get_exchange_rates()
 print(a)
+m = a['rates'].get("AED")
+print(m)
