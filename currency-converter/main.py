960b9719375f71c84227727b08db4864a43f8ea1
pavitra108 <pavitra.viswesh5@gmail.com>
2025-02-08 13:02:43 -0500

    api, html and more done

--- currency-converter/main.py
+++ currency-converter/main.py
@@ -1,6 +1,7 @@
+from flask import Flask, render_template, request, send_file
 import requests
+import json
 import os
-
 
 def get_exchange_rates():
     app_id = os.getenv('APP_ID')
@@ -10,7 +11,29 @@ def get_exchange_rates():
 
     response = requests.get(url, headers=headers)
 
-    return response.text
+    return response.json()
 
-a = get_exchange_rates()
-print(a)
+
+# The application code
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('converter-page.html')
+
+
+@app.route('/convert', methods=['POST'])
+def abc():
+    if request.method == "POST":
+        from_currency = request.form["from_currency"]
+        to_currency = request.form["to_currency"]
+        amount = float(request.form["amount"])
+
+        # Convert the amount
+        converted_amount = amount * (currency_rates[to_currency] / currency_rates[from_currency])
+
+        return render_template("converter-page.html", converted_amount=converted_amount,
+                               from_currency=from_currency, to_currency=to_currency, amount=amount)
+
+
