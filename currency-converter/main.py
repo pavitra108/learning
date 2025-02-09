093bd13932d97caa95190494ccd1e5685c70d95b
pavitra108 <pavitra.viswesh5@gmail.com>
2025-02-09 13:21:37 -0500

    app complete

--- currency-converter/main.py
+++ currency-converter/main.py
@@ -24,11 +24,14 @@ def convert_currency():
     to_currency_rates = value['rates'].get(t_currency, None)
 
     # Convert the amount
-    converted_amount = amount * (to_currency_rates/from_currency_rates)
-    print(f"{amount} {f_currency} is equal to {converted_amount} {t_currency}")
+    if from_currency_rates and to_currency_rates:
+        converted_amount = amount * (to_currency_rates / from_currency_rates)
+        converted_amount = round(converted_amount, 2) if converted_amount is not None else None
+    else:
+        converted_amount = None  # Handle invalid currency codes
+
     return render_template("converter-page.html", converted_amount=converted_amount,
                            from_currency=f_currency, to_currency=t_currency, amount=amount)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
