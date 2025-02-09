b629c040a48cf63993b63b02c9ea483efbfe7f32
pavitra108 <pavitra.viswesh5@gmail.com>
2025-02-09 13:40:27 -0500

    app complete, doc pending

--- currency-converter/rates.py
+++ currency-converter/rates.py
@@ -3,8 +3,8 @@ import json
 import os
 
 def get_exchange_rates():
-    app_id = os.getenv('APP_ID')
-    url = f"https://openexchangerates.org/api/latest.json?app_id=d69755469a4c4784a02221e1342762e7"
+    app_id = os.getenv('open_exchange_APP_ID')
+    url = f"https://openexchangerates.org/api/latest.json?app_id={app_id}"
     headers = {"accept": "application/json"}
 
     response = requests.get(url, headers=headers)
